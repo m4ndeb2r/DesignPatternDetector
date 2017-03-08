f3f1f00755e0759b6552362d51f833496d0b5534
martindeboer <m4ndeb2r@gmail.com>
2017-03-08 20:03:48 +0100

    JavaDoc added.

--- src/main/java/nl/ou/dpd/domain/Solution.java
+++ src/main/java/nl/ou/dpd/domain/Solution.java
@@ -1,12 +1,12 @@
 package nl.ou.dpd.domain;
 
 import java.util.Set;
-import java.util.SortedSet;
 
 /**
  * A {@link Solution} is the result of a matching process between a "system under consideration" and one design pattern.
  * If a design pattern is detected, a {@link Solution} contains the {@link Clazz}es from the "system under
- * consideration" that match that design pattern.
+ * consideration" that match that design pattern, the superfluous edges, and the missing edges. It provides feedback
+ * information for the user of the application.
  *
  * @author E.M. van Doorn
  * @author Martin de Boer
@@ -19,12 +19,12 @@ public class Solution {
     private final Set<Edge> missingEdges;
 
     /**
-     * Constructs a {@link Solution} instance with a set of {@code classes} (representing vertices in a graph). Two
-     * {@link Solutions} are considered "equal" if they posess the same classes/interfaces. Because the {@code classes}
-     * are passed as a {@link SortedSet}, the classes will always be in the right order (ordered by class name).
-     * TODO....
+     * Constructs a {@link Solution} instance for a design pattern.
      *
-     * @param classes a set of classes/interfaces. These classes are part of a matching matchedClasses.
+     * @param designPatternName the name of the design pattern
+     * @param matchedClasses    the matched classes
+     * @param superfluousEdges  the superfluous edges (not stricktly belonging to the desin pattern)
+     * @param missingEdges      the missing edges (that should be in the system design, but were not detected
      */
     public Solution(
             String designPatternName,
