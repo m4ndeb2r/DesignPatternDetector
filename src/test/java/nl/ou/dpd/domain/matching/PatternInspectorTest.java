f82f72fd5c3f21e6815d06b77e7826bcc5c91ade
martindeboer <m4ndeb2r@gmail.com>
2017-07-01 15:26:39 +0200

    Extended PatternInspectorTest.

--- src/test/java/nl/ou/dpd/domain/matching/PatternInspectorTest.java
+++ src/test/java/nl/ou/dpd/domain/matching/PatternInspectorTest.java
@@ -10,6 +10,12 @@ import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
+import java.util.List;
+
+import static junit.framework.TestCase.assertFalse;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -22,11 +28,19 @@ import static org.mockito.Mockito.when;
 @RunWith(MockitoJUnitRunner.class)
 public class PatternInspectorTest {
 
-    @Mock
     private SystemUnderConsideration system;
+    private DesignPattern designPattern;
 
     @Mock
-    private DesignPattern designPattern;
+    private Node systemNode1, systemNode2;
+    @Mock
+    private Relation systemRelation;
+    @Mock
+    private DesignPattern designPatternMock;
+    @Mock
+    private Node patternNode1, patternNode2;
+    @Mock
+    private Relation patternRelation;
 
     @Mock
     private FeedbackEnabledComparator<Node> nodeComparator;
@@ -35,15 +49,47 @@ public class PatternInspectorTest {
     private FeedbackEnabledComparator<Relation> relationComparator;
 
     @Before
-    public void initMockObjects() {
-        when(designPattern.getNodeComparator()).thenReturn(nodeComparator);
-        when(designPattern.getRelationComparator()).thenReturn(relationComparator);
+    public void setUp() {
+        system = new SystemUnderConsideration("sysId", "sysName");
+        system.addVertex(systemNode1);
+        system.addVertex(systemNode2);
+        system.addEdge(systemNode1, systemNode2, systemRelation);
+
+        designPattern = new DesignPattern("patternName", "patternFamily");
+        designPattern.addVertex(patternNode1);
+        designPattern.addVertex(patternNode2);
+        designPattern.addEdge(patternNode1, patternNode2, patternRelation);
     }
 
     @Test
     public void testConstructor() {
-        new PatternInspector(system, designPattern);
-        verify(designPattern, times(1)).getNodeComparator();
-        verify(designPattern, times(1)).getRelationComparator();
+        new PatternInspector(system, designPatternMock);
+        // Test if the comparators are retrieved to pass to super()
+        verify(designPatternMock, times(1)).getNodeComparator();
+        verify(designPatternMock, times(1)).getRelationComparator();
     }
+
+    @Test
+    public void testNotAnalysed() {
+        // These comparators reject any node and any relation. Nothing is therefore analysed.
+        when(nodeComparator.compare(any(Node.class), any(Node.class))).thenReturn(-1);
+        designPattern.setNodeComparator(nodeComparator);
+        when(relationComparator.compare(any(Relation.class), any(Relation.class))).thenReturn(-1);
+        designPattern.setRelationComparator(relationComparator);
+
+        final PatternInspector patternInspector = new PatternInspector(system, designPattern);
+
+        // Check solutions
+        final List<Solution> solutions = patternInspector.getSolutions();
+        assertThat(solutions.size(), is(0));
+        assertFalse(patternInspector.isomorphismExists());
+
+        // Check feedback
+        final Feedback feedback = patternInspector.getFeedback();
+        assertThat(feedback.getFeedbackMessages(systemNode1, FeedbackType.NOT_ANALYSED).size(), is(1));
+        assertThat(feedback.getFeedbackMessages(systemNode2, FeedbackType.NOT_ANALYSED).size(), is(1));
+        assertThat(feedback.getFeedbackMessages(systemRelation, FeedbackType.NOT_ANALYSED).size(), is(1));
+    }
+
+
 }
