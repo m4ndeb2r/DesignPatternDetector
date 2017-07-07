7a129b7f5daeb31085abe653aca5f6033f5abff3
martindeboer <m4ndeb2r@gmail.com>
2017-07-07 16:03:51 +0200

    Added ABSTRACT_CLASS_OR_INTERFACE to NodeType.

--- src/main/java/nl/ou/dpd/domain/node/NodeType.java
+++ src/main/java/nl/ou/dpd/domain/node/NodeType.java
@@ -4,6 +4,7 @@ public enum NodeType {
     CONCRETE_CLASS,
     ABSTRACT_CLASS,
     INTERFACE,
+    ABSTRACT_CLASS_OR_INTERFACE,
     CLASS_WITH_PRIVATE_CONSTRUCTOR, // TODO: do we still want this? Reconsider this....
     DATATYPE
 };
