2d669b84ab0fe9c8712ddc214cd28bdbbf0bea6a
martindeboer <m4ndeb2r@gmail.com>
2017-08-04 22:49:34 +0200

    Removed code smells from VisibilityTest.

--- dpd-application/src/test/java/nl/ou/dpd/domain/node/VisibilityTest.java
+++ dpd-application/src/test/java/nl/ou/dpd/domain/node/VisibilityTest.java
@@ -1,8 +1,6 @@
 package nl.ou.dpd.domain.node;
 
-import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.ExpectedException;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
@@ -14,9 +12,6 @@ import static org.hamcrest.core.Is.is;
  */
 public class VisibilityTest {
 
-    @Rule
-    public ExpectedException thrown = ExpectedException.none();
-
     @Test
     public void testValueOfIgnoreCase() {
         for (Visibility visibility : Visibility.values()) {
@@ -27,12 +22,5 @@ public class VisibilityTest {
             assertThat(Visibility.valueOfIgnoreCase(null), is((Visibility)null));
         }
     }
-
-    @Test
-    public void testValueOf() {
-        assertThat(Visibility.valueOf("PUBLIC"), is(Visibility.PUBLIC));
-        thrown.expect(IllegalArgumentException.class);
-        thrown.expectMessage("No enum");
-        Visibility.valueOf("public");
-    }
+    
 }
