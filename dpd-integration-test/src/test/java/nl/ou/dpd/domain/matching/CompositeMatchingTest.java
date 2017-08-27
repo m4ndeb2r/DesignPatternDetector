b7034b991c13ded0ac9ba950a0f928009fa9e636
martindeboer <m4ndeb2r@gmail.com>
2017-08-27 09:49:26 +0200

    Added CompositeMatchingTest.

---
+++ dpd-integration-test/src/test/java/nl/ou/dpd/domain/matching/CompositeMatchingTest.java
@@ -0,0 +1,87 @@
+package nl.ou.dpd.domain.matching;
+
+import nl.ou.dpd.IntegrationTest;
+import nl.ou.dpd.domain.DesignPattern;
+import nl.ou.dpd.domain.SystemUnderConsideration;
+import nl.ou.dpd.domain.node.Node;
+import nl.ou.dpd.domain.relation.Relation;
+import nl.ou.dpd.parsing.ArgoUMLParser;
+import nl.ou.dpd.parsing.ParserFactory;
+import nl.ou.dpd.parsing.PatternsParser;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+
+import java.net.URL;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Test the matching process for a Builder pattern.
+ *
+ * @author Martin de Boer
+ */
+@Category(IntegrationTest.class)
+public class CompositeMatchingTest extends AbstractMatchingTest {
+
+    private static final String MATCHING_SYSTEM_XMI = "/systems/MyComposite.xmi";
+    private static final String MISMATCHING_SYSTEM_XMI = "/systems/MyBuilder.xmi";
+    private static final String PATTERN_NAME = "Composite";
+
+    private DesignPattern designPattern;
+
+    @Before
+    public void initTests() {
+        final PatternsParser patternsParser = ParserFactory.createPatternParser();
+        final String patternsXmlFile = CompositeMatchingTest.class.getResource(TEMPLATES_XML).getFile();
+        designPattern = getDesignPatternByName(patternsParser.parse(patternsXmlFile), PATTERN_NAME);
+    }
+
+    @Test
+    public void testMatchingCommposite() {
+        final ArgoUMLParser xmiParser = ParserFactory.createArgoUMLParser();
+        final URL sucXmiUrl = CompositeMatchingTest.class.getResource(MATCHING_SYSTEM_XMI);
+        final SystemUnderConsideration system = xmiParser.parse(sucXmiUrl);
+
+        // Check for a general note regarding the pattern, available from the xml-file
+        assertEqualNotes(designPattern, new String[]{});
+
+        final PatternInspector patternInspector = new PatternInspector(system, designPattern);
+        assertTrue(patternInspector.isomorphismExists());
+        assertMatchingSolutions(patternInspector.getMatchingResult());
+        assertMatchingFeedback(patternInspector.getMatchingResult(), designPattern);
+    }
+
+    @Test
+    public void testMismatchingComposite() {
+        final ArgoUMLParser xmiParser = ParserFactory.createArgoUMLParser();
+        final URL sucXmiUrl = CompositeMatchingTest.class.getResource(MISMATCHING_SYSTEM_XMI);
+        final SystemUnderConsideration system = xmiParser.parse(sucXmiUrl);
+
+        final PatternInspector patternInspector = new PatternInspector(system, designPattern);
+        assertFalse(patternInspector.isomorphismExists());
+
+        final Set<Relation> relations = system.edgeSet();
+        final Set<Node> nodes = system.vertexSet();
+        final Feedback feedback = patternInspector.getMatchingResult().getFeedback();
+        assertMinimumFailedMatches(feedback, nodes, relations, 2);
+    }
+
+    private void assertMatchingSolutions(PatternInspector.MatchingResult matchingResult) {
+        final List<Solution> solutions = matchingResult.getSolutions();
+        assertEquals(3, solutions.size());
+
+        assertMatchingNodes(solutions, "Picture", "Composite");
+        assertMatchingNodes(solutions, "Graphic", "Component");
+        assertMatchingNodes(solutions, "MyUser", "Client");
+        assertAnyMatchingNode(solutions,
+                Arrays.asList(new String[]{"Line", "Rectangle", "Text"}),
+                Arrays.asList(new String[]{"Leaf"}));
+    }
+
+}
