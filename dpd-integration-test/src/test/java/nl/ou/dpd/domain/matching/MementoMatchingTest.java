a5767155ab124f41eb5e98f5b0c98ff70e1b9801
martindeboer <m4ndeb2r@gmail.com>
2017-08-27 13:03:39 +0200

    Cleaned up unused files. Organised imports.

--- dpd-integration-test/src/test/java/nl/ou/dpd/domain/matching/MementoMatchingTest.java
+++ dpd-integration-test/src/test/java/nl/ou/dpd/domain/matching/MementoMatchingTest.java
@@ -8,7 +8,6 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 
-import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
