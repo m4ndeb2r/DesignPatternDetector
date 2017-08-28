2fab4cc3c209d78994c57bb845ea3630d087c1c9
martindeboer <m4ndeb2r@gmail.com>
2017-08-28 10:28:45 +0200

    Removed CLASS_WITH_PRIVATE_CONSTRUCTOR from NodeType and XSD.

--- dpd-application/src/main/java/nl/ou/dpd/domain/node/NodeType.java
+++ dpd-application/src/main/java/nl/ou/dpd/domain/node/NodeType.java
@@ -11,6 +11,5 @@ public enum NodeType {
     ABSTRACT_CLASS,
     INTERFACE,
     ABSTRACT_CLASS_OR_INTERFACE,
-    CLASS_WITH_PRIVATE_CONSTRUCTOR, // TODO: do we still want this? Reconsider this....
     DATATYPE
 };
